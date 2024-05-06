9040aae82fb1db2ecb3e3942bee65c18b0525d67
RodrigoAVargasHdz <vargashr@mcmaster.ca>
2024-05-06 15:49:56 -0400

    pip-nn

---
+++ pipjax/pipnn_flax.py
@@ -0,0 +1,56 @@
+from typing import Any, Callable, Tuple
+
+import jax
+import jax.numpy as jnp
+from jax import jit, lax, vmap
+from jaxtyping import Array, Float, PyTree
+
+import flax
+from flax import linen as nn
+
+from pipjax.utils import all_distances, softplus_inverse, morse_variables
+from pipjax.pip_flax import PIPlayer
+
+
+class MLP(nn.Module):
+    features: Tuple[int]
+    act_fun: Callable = nn.tanh
+
+    def setup(self):
+        self.layers = [nn.Dense(feat)
+                       for feat in self.features]  # [1:]
+        self.last_layer = nn.Dense(1)
+
+    @nn.compact
+    def __call__(self, x):
+
+        z = x
+        for i, lyr in enumerate(self.layers):
+            z = lyr(z)
+            z = self.act_fun(z)
+
+        return self.last_layer(z)
+
+
+class PIPNN(nn.Module):
+    f_mono: Callable
+    f_poly: Callable
+    features: Tuple[int]
+    l: float = float(1.)
+    act_fun: Callable = nn.tanh
+
+    def setup(self):
+        self.layers = [nn.Dense(feat)
+                       for feat in self.features]  # [1:]
+        self.last_layer = nn.Dense(1)
+        self.pip_layer = PIPlayer(self.f_mono, self.f_poly, self.l,)
+
+    @nn.compact
+    def __call__(self, x):
+
+        z = self.pip_layer(x)
+        for i, lyr in enumerate(self.layers):
+            z = lyr(z)
+            z = self.act_fun(z)
+
+        return self.last_layer(z)
