0a3f64ef4715082fe78342c6d3df2921c956182f
RodrigoAVargasHdz <vargashr@mcmaster.ca>
2024-05-16 16:43:38 -0400

    pipx

---
+++ pipx/pip_flax.py
@@ -0,0 +1,118 @@
+from typing import Any, Callable
+
+import jax
+import jax.numpy as jnp
+from jax import jit, lax, vmap
+from jaxtyping import Array, Float, PyTree
+
+import flax
+from flax import linen as nn
+
+from pipx.utils import all_distances, softplus_inverse, morse_variables
+
+
+@nn.jit
+class PIP(nn.Module):
+    """Permutationally Invariant Polynomials layer.
+
+    Attributes:
+        f_mono: callable function that returns the monomials
+        f_poly: callable function that returns the polynomials
+        l: initial value of the morse variables length scale parameter
+        bias_init: initializer function for the the l parameter
+    """
+    f_mono: Callable
+    f_poly: Callable
+    l: float = float(1.)
+    bias_init: Callable = nn.initializers.constant
+
+    @nn.compact
+    def __call__(self, input):
+        """Applies the PIP transformation to the inputs.
+        (warning: this function only works for a single input)
+
+        Args:
+            inputs: geometry, array of number of atoms by 3 (Na x 3)
+
+        Returns:
+            pip_vector: pip vector
+        """
+        f_mono = self.f_mono
+        f_poly = self.f_poly
+        _lambda = self.param('lambda',
+                             # Initialization function
+                             self.bias_init(softplus_inverse(self.l)),
+                             (1,))  # shape info.
+        l = nn.softplus(_lambda)  # positive initialization
+
+        d = all_distances(input)  # compute distances
+        morse = jnp.exp(-l*d)  # comput morse variables
+
+        # mono = f_mono(morse)
+        # compute PIP vector, morse is computed inside f_pip
+        pip = f_poly(morse)
+        return pip
+
+
+@nn.jit
+class PIPlayer(nn.Module):
+    """Vectorize version of ``PIP``.
+    see ``PIP`` for more information.
+
+    Attributes:
+        f_mono: callable function that returns the monomials
+        f_poly: callable function that returns the polynomials
+        l: initial value of the morse variables length scale parameter
+    """
+    f_mono: Callable
+    f_poly: Callable
+    l: float = float(jnp.exp(1))
+
+    @nn.compact
+    def __call__(self, inputs):
+        """Applies a vectorize map to PIP.apply.
+
+        Args:
+            inputs: geometries, (batch,number of atoms, 3)
+
+        Returns:
+            pip_vector: pip vector for each geometry, (batch, number of pips)
+        """
+        vmap_pipblock = nn.vmap(PIP, variable_axes={'params': None, },
+                                split_rngs={'params': False, },
+                                in_axes=(0,))(self.f_mono, self.f_poly, self.l)
+
+        return vmap_pipblock(inputs)
+
+
+@nn.jit
+class EnergyPIP(nn.Module):
+    """Energy model using PIP."
+
+    Attributes:
+        f_mono: callable function that returns the monomials
+        f_poly: callable function that returns the polynomials
+        l: initial value of the morse variables length scale parameter
+    """
+    f_mono: Callable
+    f_poly: Callable
+    l: float = float(jnp.exp(1))
+
+    @nn.compact
+    def __call__(self, inputs):
+        """Applies the ``PIPLayer`` and a ``nn.Dense`` modules to compute the energy.
+
+        Args:
+            inputs: geometries, (batch,number of atoms, 3)
+
+        Returns:
+            energy: energy of each geometry, (batch, 1)
+        """
+        vmap_pipblock = nn.vmap(PIP, variable_axes={'params': None, },
+                                split_rngs={'params': False, },
+                                in_axes=(0,))(self.f_mono, self.f_poly, self.l)
+        layer = nn.Dense(1, use_bias=False)
+
+        pip = vmap_pipblock(inputs)  # computes the pip vectors
+        energy = layer(pip)  # linear layer
+        return energy
---
+++ pipx/training_linear_model.py
@@ -0,0 +1,82 @@
+from typing import Callable
+import jax
+import jax.numpy as jnp
+from jax import lax, jit
+from jaxtyping import Array, Float, PyTree, Key
+
+from pipx.utils_gradients import get_pip_grad
+
+
+def training(model_pip: Callable,
+             X_tr: Float[Array, "..."],
+             y_tr: Float[Array, "..."]) -> Float[Array, "..."]:
+    """Simple training function for PIP models.
+    Warning: Geometries must be in Bohr Units
+
+    Args:
+        model_pip: Callable, Flax class to compute the PIP vectors
+        X_tr: Training Geometries, (batch,number of atoms, 3) 
+        y_tr: Training Energies, (batch,1)
+
+    Returns:
+        Array: Optimal linear parameters
+    """
+    rng = jax.random.PRNGKey(0)
+    _, key = jax.random.split(rng)
+
+    xyz0 = X_tr[0]  # single point to initialize the PIP model
+    params_pip = model_pip.init(key, xyz0[jnp.newaxis])
+
+    # PIP matrix training
+    Pip_tr = model_pip.apply(params_pip, X_tr)
+
+    # Solving the linear system of equations  PIP x = Energy
+    results = jnp.linalg.lstsq(Pip_tr, y_tr)
+
+    theta = results[0]
+    return theta
+
+
+def training_w_gradients(model_pip: Callable,
+                         X_tr: Float[Array, "..."],
+                         F_tr: Float[Array, "..."],
+                         y_tr: Float[Array, "..."]) -> Float[Array, "..."]:
+    """Simple training function for PIP models with Forces.
+    Warning: Geometries must be in Bohr Units and Forces in Ha/Bohr Units
+
+    Args:
+        model_pip: Callable, Flax class to compute the PIP vectors
+        X_tr: Training Geometries, (batch,number of atoms, 3) 
+        F_tr: Training Forces, (batch, number of atoms, 3)
+        y_tr: Training Energies, (batch,1)
+
+    Returns:
+       Array: Optimal linear parameters
+    """
+
+    n, n_atoms, _ = X_tr.shape
+
+    rng = jax.random.PRNGKey(0)
+    _, key = jax.random.split(rng)
+
+    xyz0 = X_tr[0]  # single point to initialize the PIP model
+    params_pip = model_pip.init(key, xyz0[jnp.newaxis])
+    x0_pip = model_pip.apply(params_pip, xyz0[jnp.newaxis])
+    n_pip = x0_pip.shape[1]
+
+    # Training PIP matrix
+    Pip_tr = model_pip.apply(params_pip, X_tr)
+    GPIP_tr = get_pip_grad(model_pip.apply, X_tr, params_pip)
+
+    # (bs*n_atoms*3,number of pip)
+    GPIP_tr_w_grad = GPIP_tr.reshape(n*n_atoms*3, n_pip)
+    Pip_tr_w_grad_full = lax.concatenate((Pip_tr, GPIP_tr_w_grad), dimension=0)
+
+    y_tr_w_grad_full = lax.concatenate(
+        (y_tr, F_tr.reshape(n*n_atoms*3, 1)), dimension=0)
+    # ---------
+
+    # Optimization
+    results_w_grad = jnp.linalg.lstsq(Pip_tr_w_grad_full, y_tr_w_grad_full)
+    theta = results_w_grad[0]
+    return theta
