6791d6dadcd88bb7d9f3065ead7fd48efd7d8e40
RodrigoAVargasHdz <vargashr@mcmaster.ca>
2026-02-10 16:08:26 -0500

    MSA files for A2

---
+++ molpipx/msa_files/molecule_A2/test.py
@@ -0,0 +1,45 @@
+import molpipx
+from molpipx import msa_file_generator
+from molpipx.utils import all_distances
+from molpipx import detect_molecule, get_functions
+
+import jax
+import jax.random as jrnd
+
+
+def generate_msa_file_generator(n_A: int = 2, poly_degree: int = 3):
+
+    # filename = f'MOL_{n_A}_{poly_degree}'
+    path = '/home/ravh011/molpipx/molpipx/msa_files/molecule_A2'
+    label = f'_MOL_{n_A}_{poly_degree}'
+    for p in range(3, 9):  # max degree generated was 6
+        filename = f'MOL_{n_A}_{p}'
+        label = f'_MOL_{n_A}_{p}'
+        msa_file_generator(filename=filename, path=path, label=label)
+
+
+def test_mono_files(poly_degree: int = 3):
+    n_a = 6
+    rng = jrnd.PRNGKey(0)
+    _, key = jrnd.split(rng)
+
+    # random geometries to test the energy and forces
+    geoms = jrnd.normal(key, (1, n_a, 3))
+    distance = all_distances(geoms[0])
+    print(f'Distances: {distance}')
+    print(distance.shape)
+
+    f_mono, f_poly = get_functions('A6', poly_degree)
+    x_mono = f_mono(distance)
+    x_poly = f_poly(distance)
+
+    print(x_mono.shape, x_poly.shape)
+
+
+if __name__ == "__main__":
+    generate_msa_file_generator()
+    # test_mono_files(poly_degree=6)
+    # assert 0
+    # test_mono_files(poly_degree=3)
+    # test_mono_files(poly_degree=4)
+    # test_mono_files(poly_degree=5)
